dd6c943f16317269c32029f6eb6224eb0ea7372d
Manny Pissanos <mpissano@gmail.com>
2018-09-26 14:15:51 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,8 +15,8 @@ def assign_rooms(names)
     arr = []
     names.each_with_index do |name, index|
       arr << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
-end
-return arr
+    end
+  return arr
 end
 
 
